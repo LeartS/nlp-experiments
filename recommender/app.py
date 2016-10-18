82e9dfdcbc299cd11696bffc97051f3fca44e99a
Leonardo Donelli <learts92@gmail.com>
2016-10-18 14:11:47 +0200

    Save matrix and other post data

--- recommender/app.py
+++ recommender/app.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
 from flask import Flask, jsonify, request
+import numpy as np
+from scipy import io
+import simplejson
+import os
 
 from recommender import HashtagRecommender
 
@@ -8,12 +12,17 @@ from recommender import HashtagRecommender
 app = Flask(__name__)
 
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
-    recommender = HashtagRecommender()
-    recommender.go(request.json['username'], request.json['hashtags'])
-    return jsonify({'ciao': [1, 2]})
-    pass
+@app.route('/matrix', methods=['POST'])
+def save_matrix():
+    hashtags = simplejson.loads(request.form['hashtags_indexed_array'])
+    m_graph_id = int(request.form['m_graph_id'])
+    matrix = io.mmread(request.files['common_tweets_file'])
+    os.mkdir('{}'.format(m_graph_id))
+    data = {'hashtags': hashtags}
+    np.save('{}/matrix.npy'.format(m_graph_id), matrix.toarray())
+    with open('{}/data.json'.format(m_graph_id), 'w') as out:
+        simplejson.dump(data, out)
+    return 'ok'
 
 
 if __name__ == '__main__':
